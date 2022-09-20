181540c6ed1292084b90c690ce663d69b727a710
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2022-09-20 19:24:12 +0300

    fix

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 import pathlib
 from dotenv import dotenv_values
+
 BASE_DIR = pathlib.Path(__file__).parent.parent
 config = dotenv_values(".env")
 
@@ -7,4 +8,5 @@ config = dotenv_values(".env")
 class Config:
     UPLOAD_FOLDER = str(BASE_DIR / 'uploads')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / 'data' / 'app.db.sqlite3')
-    SECRET_KEY = config['SECRET_KEY']
+    # SECRET_KEY = config['SECRET_KEY']
+    SECRET_KEY = 'super_secret_app_007'
